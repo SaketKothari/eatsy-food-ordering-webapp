62d02e81a9192406484b5ab66c26a294b9f3f224
Saket Kothari <saketkothari722@gmail.com>
2023-10-06 22:34:53 +0530

    feat: filtering resta. with avgRating using useState

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,56 @@
+import { useState } from 'react';
+
 import { resList } from '../utils/mockData';
 import RestaurantCard from './RestaurantCard';
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState([
+    {
+      data: {
+        id: '334475',
+        name: 'KFC',
+        cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
+        cuisines: ['Burgers', 'Biryani', 'American', 'Snacks', 'Fast Food'],
+        costForTwo: 40000,
+        deliveryTime: 36,
+        avgRating: '3.8',
+      },
+    },
+    {
+      data: {
+        id: '334476',
+        name: 'Dominos',
+        cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
+        cuisines: ['Pizza', 'American', 'Snacks', 'Fast Food'],
+        costForTwo: 40000,
+        deliveryTime: 36,
+        avgRating: '4.5',
+      },
+    },
+  ]);
+
   return (
     <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            // Filter logic
+            setListOfRestaurants();
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+
       <div className="search">Search</div>
       <div className="res-container">
-        {resList.map((restaurant, id) => (
-          <RestaurantCard key={id} resData={restaurant} />
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
     </div>
