c855381cf7e2bcea67f46eea79ce77b8ac5a59d3
Saket Kothari <saketkothari722@gmail.com>
2023-10-06 18:45:23 +0530

    implement nested element structure

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-const parent = React.createElement(
-  'div',
-  { id: 'parent' },
-  React.createElement('div', { id: 'child' }, [
-    React.createElement('h1', {}, "I'm Batman"),
-    React.createElement('h2', {}, "I'm Superman"),
-  ])
-);
+const parent = React.createElement('div', { id: 'parent' }, [
+  React.createElement('div', { id: 'child1' }, [
+    React.createElement('h1', {}, 'Batman'),
+    React.createElement('h2', {}, 'Superman'),
+  ]),
+  React.createElement('div', { id: 'child2' }, [
+    React.createElement('h1', {}, 'Bruce Wayne'),
+    React.createElement('h2', {}, 'Clark Kent'),
+  ]),
+]);
 
 console.log(parent); // Object
 const root = ReactDOM.createRoot(document.getElementById('root'));
