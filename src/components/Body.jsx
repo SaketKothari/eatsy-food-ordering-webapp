5f7363c9cc285614cbc023a5a04983eaf93b3103
Saket Kothari <saketkothari722@gmail.com>
2023-10-06 22:36:24 +0530

    fix: passing the mock data inside useState

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,30 +4,7 @@ import { resList } from '../utils/mockData';
 import RestaurantCard from './RestaurantCard';
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState([
-    {
-      data: {
-        id: '334475',
-        name: 'KFC',
-        cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
-        cuisines: ['Burgers', 'Biryani', 'American', 'Snacks', 'Fast Food'],
-        costForTwo: 40000,
-        deliveryTime: 36,
-        avgRating: '3.8',
-      },
-    },
-    {
-      data: {
-        id: '334476',
-        name: 'Dominos',
-        cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
-        cuisines: ['Pizza', 'American', 'Snacks', 'Fast Food'],
-        costForTwo: 40000,
-        deliveryTime: 36,
-        avgRating: '4.5',
-      },
-    },
-  ]);
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
 
   return (
     <div className="body">
