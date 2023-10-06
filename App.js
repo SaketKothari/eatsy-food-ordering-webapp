d6d659fbb54f638feb1bc89ae78540c1ef9a04d8
Saket Kothari <saketkothari722@gmail.com>
2023-10-06 18:43:36 +0530

    creating a sibling like structure

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 const parent = React.createElement(
   'div',
   { id: 'parent' },
-  React.createElement(
-    'div',
-    { id: 'child' },
-    React.createElement('h1', {}, "I'm Batman")
-  )
+  React.createElement('div', { id: 'child' }, [
+    React.createElement('h1', {}, "I'm Batman"),
+    React.createElement('h2', {}, "I'm Superman"),
+  ])
 );
 
 console.log(parent); // Object
