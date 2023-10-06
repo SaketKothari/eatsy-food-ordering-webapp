922d4990d8d77eb6b655c89c37562b54f512531a
Saket Kothari <saketkothari722@gmail.com>
2023-10-06 20:05:03 +0530

    implement restaurant card

--- App.js
+++ App.js
@@ -20,10 +20,42 @@ const Header = () => {
   );
 };
 
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={{ backgroundColor: '#f0f0f0' }}>
+      <img
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ysvhwgn1iluc4j4mmvnc"
+        alt="restaurant-image"
+      />
+      <h3>La Pinoz Pizza</h3>
+      <h4>Biryani, North-Indian</h4>
+      <h4>4.3 stars</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
