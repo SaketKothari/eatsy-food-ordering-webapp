f4dfc803227de3e4d551ec91e34445597231424d
Saket Kothari <saketkothari722@gmail.com>
2023-10-06 18:42:04 +0530

    creating nested elements

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
-const heading = React.createElement(
-  'h1',
-  { id: 'heading' },
-  'Hello World from React!'
+const parent = React.createElement(
+  'div',
+  { id: 'parent' },
+  React.createElement(
+    'div',
+    { id: 'child' },
+    React.createElement('h1', {}, "I'm Batman")
+  )
 );
+
+console.log(parent); // Object
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(parent);
