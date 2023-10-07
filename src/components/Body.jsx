e974e82361ecf25ad69ce2c25a805c6f466f0d04
Saket Kothari <saketkothari722@gmail.com>
2023-10-07 11:33:42 +0530

    custom hook for fetching the restaurants

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,61 +1,20 @@
-import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-import { FETCH_RESTAURANT_URL } from '../utils/constants';
 import RestaurantCard from './RestaurantCard';
 import Shimmer from './Shimmer';
+import useRestaurant from '../hooks/useRestaurant';
 
 const Body = () => {
-  const [filterRestaurant, setFilterRestaurant] = useState([]);
-  const [listOfRestaurants, setListOfRestaurants] = useState([]);
-  const [notFound, setNotFound] = useState(false);
-  const [searchText, setSearchText] = useState('');
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    try {
-      const data = await fetch(FETCH_RESTAURANT_URL);
-      const json = await data.json();
-      const restaurants =
-        json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
-          ?.restaurants;
-      setListOfRestaurants(restaurants);
-      setFilterRestaurant(restaurants);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-
-  const handleSearch = () => {
-    const filteredRestaurant = listOfRestaurants.filter((res) =>
-      res.info.name.toLowerCase().includes(searchText.toLowerCase())
-    );
-    setFilterRestaurant(filteredRestaurant);
-
-    if (filteredRestaurant.length === 0) {
-      setNotFound(true);
-    } else {
-      setNotFound(false);
-    }
-    setSearchText('');
-  };
-
-  const topRatedRestaurants = () => {
-    const filteredList = listOfRestaurants.filter(
-      (res) => res.info.avgRating > 4
-    );
-    setFilterRestaurant(filteredList);
-    setNotFound(false);
-  };
-
-  const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
-      handleSearch();
-    }
-  };
+  const {
+    filterRestaurant,
+    handleKeyPress,
+    handleSearch,
+    listOfRestaurants,
+    notFound,
+    searchText,
+    setSearchText,
+    topRatedRestaurants,
+  } = useRestaurant();
 
   return listOfRestaurants.length === 0 ? (
     <Shimmer />
---
+++ src/hooks/useRestaurant.js
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react';
+import { FETCH_RESTAURANT_URL } from '../utils/constants';
+
+const useRestaurant = () => {
+  const [filterRestaurant, setFilterRestaurant] = useState([]);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [notFound, setNotFound] = useState(false);
+  const [searchText, setSearchText] = useState('');
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const data = await fetch(FETCH_RESTAURANT_URL);
+      const json = await data.json();
+      const restaurants =
+        json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants;
+      setListOfRestaurants(restaurants);
+      setFilterRestaurant(restaurants);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const handleSearch = () => {
+    const filteredRestaurant = listOfRestaurants.filter((res) =>
+      res.info.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilterRestaurant(filteredRestaurant);
+
+    if (filteredRestaurant.length === 0) {
+      setNotFound(true);
+    } else {
+      setNotFound(false);
+    }
+    setSearchText('');
+  };
+
+  const topRatedRestaurants = () => {
+    const filteredList = listOfRestaurants.filter(
+      (res) => res.info.avgRating > 4
+    );
+    setFilterRestaurant(filteredList);
+    setNotFound(false);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  return {
+    filterRestaurant,
+    handleKeyPress,
+    handleSearch,
+    listOfRestaurants,
+    notFound,
+    searchText,
+    setSearchText,
+    topRatedRestaurants,
+  };
+};
+
+export default useRestaurant;
