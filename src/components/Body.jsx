942db9e3c0e6c4d2f4180462ae6b7e2b324c0f93
Saket Kothari <saketkothari722@gmail.com>
2023-10-07 09:32:41 +0530

    refactor: enhance code structure & error handling

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,6 +7,7 @@ import Shimmer from './Shimmer';
 const Body = () => {
   const [filterRestaurant, setFilterRestaurant] = useState([]);
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [notFound, setNotFound] = useState(false);
   const [searchText, setSearchText] = useState('');
 
   useEffect(() => {
@@ -14,14 +15,45 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch(FETCH_RESTAURANT_URL);
-    const json = await data.json();
-    setListOfRestaurants(
-      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    try {
+      const data = await fetch(FETCH_RESTAURANT_URL);
+      const json = await data.json();
+      const restaurants =
+        json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants;
+      setListOfRestaurants(restaurants);
+      setFilterRestaurant(restaurants);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const handleSearch = () => {
+    const filteredRestaurant = listOfRestaurants.filter((res) =>
+      res.info.name.toLowerCase().includes(searchText.toLowerCase())
     );
-    setFilterRestaurant(
-      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    setFilterRestaurant(filteredRestaurant);
+
+    if (filteredRestaurant.length === 0) {
+      setNotFound(true);
+    } else {
+      setNotFound(false);
+    }
+    setSearchText('');
+  };
+
+  const topRatedRestaurants = () => {
+    const filteredList = listOfRestaurants.filter(
+      (res) => res.info.avgRating > 4
     );
+    setFilterRestaurant(filteredList);
+    setNotFound(false);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
   };
 
   return listOfRestaurants.length === 0 ? (
@@ -39,44 +71,29 @@ const Body = () => {
             onChange={(e) => {
               setSearchText(e.target.value);
             }}
+            onKeyUp={handleKeyPress}
           />
-          <button
-            onClick={() => {
-              // Filter the restaurant cards & update the UI
-              const filteredRestaurant = listOfRestaurants.filter((res) =>
-                res.info.name.toLowerCase().includes(searchText.toLowerCase())
-              );
-              setFilterRestaurant(filteredRestaurant);
-            }}
-            className="search-btn"
-          >
+          <button onClick={handleSearch} className="search-btn">
             Search
           </button>
         </div>
 
-        <button
-          className="filter-btn"
-          onClick={() => {
-            // Filter logic
-            setListOfRestaurants();
-            const filteredList = listOfRestaurants.filter(
-              (res) => res.info.avgRating > 4
-            );
-            setListOfRestaurants(filteredList);
-          }}
-        >
+        <button className="filter-btn" onClick={topRatedRestaurants}>
           Top Rated Restaurants
         </button>
       </div>
 
-      <div className="search">Search</div>
       <div className="res-container">
-        {filterRestaurant.map((restaurant) => (
-          <RestaurantCard
-            key={restaurant?.info.id}
-            resData={restaurant?.info}
-          />
-        ))}
+        {filterRestaurant.length === 0 && notFound ? (
+          <p>No results found</p>
+        ) : (
+          filterRestaurant.map((restaurant) => (
+            <RestaurantCard
+              key={restaurant?.info.id}
+              resData={restaurant?.info}
+            />
+          ))
+        )}
       </div>
     </div>
   );
