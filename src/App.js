22ba9f94fee5da5353b5c9e69a01fc20b0615aa1
Saket Kothari <saketkothari722@gmail.com>
2023-10-07 10:01:41 +0530

    made a error component for routing errors

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 
 import Body from './components/Body';
+import Error from './components/Error';
 import Header from './components/Header';
 
 const AppLayout = () => {
@@ -21,6 +22,7 @@ const appRouter = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <Error />,
   },
   {
     path: '/about',
