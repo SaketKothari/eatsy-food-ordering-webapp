e86f825abe67a65739be7f5f8f00081edb1efe35
Saket Kothari <saketkothari722@gmail.com>
2023-10-08 09:53:36 +0530

    provide the store to our app and creating slice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { lazy, Suspense, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -12,6 +13,7 @@ import Header from './components/Header';
 const Instamart = lazy(() => import('./components/Instamart'));
 import RestaurantMenu from './components/RestaurantMenu';
 
+import appStore from './store/appStore';
 import UserContext from './context/UserContext';
 
 const AppLayout = () => {
@@ -26,13 +28,15 @@ const AppLayout = () => {
   }, []);
 
   return (
-    <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
-      <div className="app">
-        <Header />
-        <Outlet />
-        <Footer />
-      </div>
-    </UserContext.Provider>
+    <Provider store={appStore}>
+      <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
+        <div className="app">
+          <Header />
+          <Outlet />
+          <Footer />
+        </div>
+      </UserContext.Provider>
+    </Provider>
   );
 };
 
---
+++ src/slices/cartSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addItem: (state, action) => {
+      state.items.push(action.payload);
+    },
+
+    removeItem: (state, action) => {
+      let pos = state.items.findIndex((item) => item.id === action.payload.id);
+      let newBasket = [...state.items];
+
+      if (pos > -1) {
+        newBasket.splice(pos, 1);
+      } else {
+        console.warn(
+          `Can't remove product (id: ${action.payload.id}) as its not in the cart`
+        );
+      }
+      state.items = newBasket;
+    },
+
+    clearCart: (state) => {
+      state.items.length = 0; // []
+    },
+  },
+});
+
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
