2e265ce73feec5ca3b73d1a2277eedcb5378dc71
Saket Kothari <saketkothari722@gmail.com>
2024-02-28 20:15:49 +0530

    bypassing the cors error

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 export const IMAGE_CDN_URL =
-  'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/';
+  'https://corsproxy.io/?https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/';
 
 export const FETCH_RESTAURANT_URL =
-  'https://www.swiggy.com/dapi/restaurants/list/v5?lat=24.579106574797905&lng=73.7353663684841&page_type=DESKTOP_WEB_LISTING';
+  'https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=24.579106574797905&lng=73.7353663684841&page_type=DESKTOP_WEB_LISTING';
 
 export const MENU_API =
-  'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=';
+  'https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=';
